7520a05da0c9328c23da754fda3fb2f32ac53eae
xbvpx <butek@inbox.ru>
2023-03-12 20:12:01 +0300

    Initial commit

--- src/Basket.java
+++ src/Basket.java
@@ -1,2 +1,80 @@
-package PACKAGE_NAME;public class Basket {
+import java.io.*;
+
+public class Basket {
+    private String[] products;
+    private int[] prices;
+    private int[] basketCount;
+
+
+    public Basket(String[] products, int[] prices) {
+        this.products = products;
+        this.prices = prices;
+        this.basketCount = new int[products.length];
+    }
+
+    public void scroll() {
+        System.out.println("Список возможных товаров для покупки");
+        for (int i = 0; i < products.length; i++) {
+            System.out.println((i + 1) + "." + products[i] + " цена: " + prices[i]);
+        }
+    }
+
+    public void addToCart(int productNum, int amount) {
+        basketCount[productNum] += amount;
+    }
+
+    public void printCart() {
+        System.out.println("Ваша корзина:");
+        for (int i = 0; i < basketCount.length; i++) {
+            System.out.println(products[i] + " " + basketCount[i] + " шт " + prices[i] + " руб/шт " + (basketCount[i] * prices[i]) + " руб в сумме");
+        }
+    }
+
+    public int sumProd() {
+        int sum = 0;
+        for (int i = 0; i < products.length; i++) {
+            sum += basketCount[i] * prices[i];
+        }
+        return sum;
+    }
+
+    public void saveTxt(File textFile) throws IOException {
+        try (PrintWriter out = new PrintWriter(textFile)) {
+            for (String product : products)
+                out.print(product + " ");
+            out.println();
+            for (int price : prices)
+                out.print(price + " ");
+            out.println();
+            for (int count : basketCount)
+                out.print(count + " ");
+        }
+    }
+
+    static Basket loadFromTxtFile(File textFile) {
+        try (FileReader reader = new FileReader(textFile)) {
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            while (bufferedReader.ready()) {
+                String[] product = bufferedReader.readLine().split(" ");
+                String[] prices1 = bufferedReader.readLine().split(" ");
+                String[] basketCount1 = bufferedReader.readLine().split(" ");
+                int[] price = new int[prices1.length];
+                for (int i = 0; i < prices1.length; i++) {
+                    price[i] = Integer.parseInt(prices1[i]);
+                }
+                int[] basketCount = new int[basketCount1.length];
+                for (int i = 0; i < basketCount1.length; i++) {
+                    basketCount[i] = Integer.parseInt(basketCount1[i]);
+                }
+
+                Basket basket = new Basket(product, basketCount);
+                basket.basketCount = basketCount;
+                return basket;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return null;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        Basket basket = new Basket(new String[]{"Хлеб", "Яблоки", "Молоко"}, new int[]{100, 200, 300});
+
+        basket.scroll();
+
+        try {
+            while (true) {
+                System.out.println("Выберите товар и количество или введите 'end'");
+                String input = scanner.nextLine();
+                if ("end".equals(input)) {
+                    break;
+                }
+                String[] parst = input.split(" ");
+                basket.addToCart(Integer.parseInt(parst[0]) - 1, Integer.parseInt(parst[1]));
+                basket.printCart();
+            }
+
+        } catch (RuntimeException exception) {
+            System.out.println("Введено не верное значение!");
+        }
+        System.out.println("Итого " + basket.sumProd() + " руб");
+
+        File textFile = new File("list.txt");
+        try {
+            if (textFile.createNewFile())
+                System.out.println("Файл был создан");
+
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        basket.saveTxt(textFile);
+        Basket.loadFromTxtFile(textFile);
     }
 }
